003811bd87584470acdfda1ac5df3829ba1fcb19
LokranshX <lokransh@gmail.com>
2025-06-14 20:37:53 +0300

    feat: Add gitignore and Actions workflow

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,22 +1,10 @@
-// frontend/src/socket.js
 import { io } from 'socket.io-client';
 
-// URL вашего рабочего бэкенда на Vercel
-const vercelBackendUrl = 'https://who-am-i-game-eight.vercel.app/';
-
-// URL для локальной разработки
-const localBackendUrl = 'http://localhost:5000';
-
-// Умное переключение между адресами:
-// process.env.NODE_ENV устанавливается автоматически:
-// - в 'production' при выполнении `npm run build` (для GitHub Pages)
-// - в 'development' при выполнении `npm start` (локально)
-const BACKEND_URL = process.env.NODE_ENV === 'production'
-  ? vercelBackendUrl
-  : localBackendUrl;
+// Во время сборки на GitHub Actions, process.env.REACT_APP_BACKEND_URL будет заменен на ваш секрет.
+// При локальной разработке он будет undefined, и мы будем использовать localhost.
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 
 export const socket = io(BACKEND_URL, {
     autoConnect: false,
-    // Важное дополнение для Vercel, чтобы избежать проблем с CORS при первом подключении
     transports: ['websocket', 'polling']
 });
